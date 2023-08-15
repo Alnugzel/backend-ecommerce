69cbde918462c18c7a26bbb5bfaecdbee3ab2875
Alnugzel <angelfiguera0889@gmail.com>
2023-08-15 15:58:12 -0400

    model product e img

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,6 @@
 const Category = require("./Category");
+const Product = require("./Product");
 const User = require("./User");
+
+Category.hasMany(Product); // categoryId
+Product.belongsTo(Category);
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,8 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
 const User = require("../models/User");
-let userId;
 
+let userId;
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjozLCJmaXJzdE5hbWUiOiJBbmdlbCIsImxhc3ROYW1lIjoiRmlndWVyYSIsImVtYWlsIjoiYW5nZWxmaWd1ZXJhMDg4OUBnbWFpbC5jb20iLCJwaG9uZSI6Iis1ODQxNDEyMzQ1NjciLCJjcmVhdGVkQXQiOiIyMDIzLTA4LTE1VDEzOjM5OjQ4LjYwOVoiLCJ1cGRhdGVkQXQiOiIyMDIzLTA4LTE1VDEzOjM5OjQ4LjYwOVoifSwiaWF0IjoxNjkyMTA2ODAxLCJleHAiOjE2OTIxOTMyMDF9.GhLBmoJYhkLgYPavorHhHsNNhEgqWKl7MqdH0_RkVig";
 const URL_USERS = "/api/v1/users";
 
 const user = {
@@ -20,5 +22,15 @@ test(`POST -> '${URL_USERS}' should return status code 201 req.body === user.fir
 
   expect(res.status).toBe(201);
   expect(res.body).toBeDefined();
-  expect(res.body.firstName).toBe(user.name);
+  expect(res.body.firstName).toBe(user.firstName);
 });
+
+// test(`POST -> '${URL_USERS}' should return status code 201 req.body === user.name`);
+
+// test(`GET -> '${URL_USERS}' should return status code 200`, async () => {
+//   const res = await request(app)
+//     .set("Autorization", "Bearer " + token)
+//     .get(URL_USERS);
+
+//   expect(res.status).toBe(200);
+// });
