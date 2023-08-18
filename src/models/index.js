2e4dfda3978bcd7004130170cb341164d82a76ca
Alnugzel <angelfiguera0889@gmail.com>
2023-08-17 23:03:20 -0400

    models cart and purchase

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,25 @@
+const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
+const Purchase = require("./Purchase");
 const User = require("./User");
 
+// categoryId
 Product.belongsTo(Category);
-Category.hasMany(Product); // categoryId
+Category.hasMany(Product);
+
+// Cart -> userId
+Cart.belongsTo(User);
+User.hasMany(Cart);
+
+// Cart -> productId
+Cart.belongsTo(Product);
+Product.hasMany(Cart);
+
+// Purchase -> productId
+Purchase.belongsTo(Product);
+Product.hasMany(Purchase);
+
+// Purchase -> userId
+Purchase.belongsTo(User);
+User.hasMany(Purchase);
